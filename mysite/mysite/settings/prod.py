f14bbbc6aa2301c267f8ce943dc7976b0277ba71
baileyrechkemmer <rechkemmer3@gmail.com>
2023-04-11 13:01:54 -0500

    portfolio properly built for heroku1 part2

--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -11,3 +11,35 @@ ALLOWED_HOSTS = [
     'baileyr-portfolio.herokuapp.com',
     'baileyr.portfolio',
 ]
+
+DEBUG_propagate_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_exsiting_logggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple' : {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': { 
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+# herocu settings 
+django_on_heroku.settings(locals(), ststicfiles=False)
+del DATABASES['default']['OPTIONS'] ['sslmode']
