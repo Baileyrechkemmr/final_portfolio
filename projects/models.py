939387814040c1d5f6c9295b81c58c8d7baa881b
baileyrechkemmer <rechkemmer3@gmail.com>
2023-04-13 14:25:59 -0500

    is deployed now adding media stuff to populate project images changed details

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=250)
-    project_image= models.ImageField(upload_to='static/css/image')
+    project_image= models.ImageField(upload_to='images/')
     description = models.TextField()
 
     def __str__(self):
