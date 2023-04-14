73abbd05266884dc194b5ba627f24f90c4825569
baileyrechkemmer <rechkemmer3@gmail.com>
2023-04-13 22:10:26 -0500

    trying top get image by url string

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=250)
-    project_image= models.ImageField()
+    project_image= models.CharField()
     description = models.TextField()
 
     def __str__(self):
